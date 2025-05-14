e8c1020851927994212ba7e4869763bf619834db
bodya <lavajover@gmail.com>
2025-05-14 22:08:10 +0300

    feat: add event user.created to usecase

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -1,16 +1,25 @@
 package usecase
 
-import "github.com/LavaJover/shvark-user-service/internal/domain"
+import (
+	"context"
+
+	"github.com/LavaJover/shvark-user-service/internal/domain"
+	"github.com/LavaJover/shvark-user-service/internal/infrastructure/kafka"
+)
 
 type UserUsecase struct {
 	Repo domain.UserRepository
+	producer kafka.EventProducer
 }
 
 func NewUserUsecase(repo domain.UserRepository) domain.UserUsecase {
-	return &UserUsecase{repo}
+	return &UserUsecase{Repo: repo}
 }
 
 func (uc *UserUsecase) CreateUser (user *domain.User) (string, error) {
+	if err := uc.producer.Produce(context.Background(), "user.created", *user); err != nil {
+		return "", err
+	}
 	return uc.Repo.CreateUser(user)
 }
 
