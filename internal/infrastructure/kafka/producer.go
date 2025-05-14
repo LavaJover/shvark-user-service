dd30f8f5fc63e9d9b6eed55d76bff6d9a9083767
bodya <lavajover@gmail.com>
2025-05-14 22:03:05 +0300

    feat: add producer

--- internal/infrastructure/kafka/producer.go
+++ internal/infrastructure/kafka/producer.go
@@ -1,6 +1,37 @@
 package kafka
 
-type UserCreated struct {
-	UserID		string `json:"user_id"`
-	Username 	string `json:"username"`
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type EventProducer interface {
+	Produce(ctx context.Context, topic string, message interface{}) error
+}
+
+type KafkaProducer struct {
+	writer *kafka.Writer
+}
+
+func NewKafkaProducer(brokerAddr string) *KafkaProducer {
+	return &KafkaProducer{
+		writer: &kafka.Writer{
+			Addr: kafka.TCP(brokerAddr),
+			Balancer: &kafka.LeastBytes{},
+		},
+	}
+}
+
+func (kp *KafkaProducer) Produce(ctx context.Context, topic string, message interface{}) error {
+	bytes, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	return kp.writer.WriteMessages(ctx, kafka.Message{
+		Topic: topic,
+		Value: bytes,
+	})
 }
