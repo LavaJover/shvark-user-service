eaad16acf026830207de88cb8c0018ed05fc013c
bodya <lavajover@gmail.com>
2025-05-16 20:00:32 +0300

    init roles to user domain

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -2,11 +2,22 @@ package domain
 
 import "time"
 
+type Role string
+
+const (
+	Admin		Role = "admin"
+	Lead		Role = "lead"
+	TeamLead	Role = "team_lead"
+	Trader		Role = "trader"
+	Marketplace	Role = "marketplace"
+)
+
 type User struct {
 	ID			string
 	Username	string
 	Login		string
 	Password	string
+	Role		Role
 	CreatedAt	time.Time
 	UpdatedAt 	time.Time
 }
--- internal/domain/user_usecase.go
+++ internal/domain/user_usecase.go
@@ -4,4 +4,5 @@ type UserUsecase interface {
 	GetUserByID(userID string) (*User, error)
 	GetUserByLogin(login string) (*User, error)
 	CreateUser(user *User) (string, error)
+	CheckPermission(userID string, required Role) (bool, error)
 }
