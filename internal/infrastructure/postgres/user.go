2bfc186d393a9a3c232179c990b6a3971e87dce4
bodya <lavajover@gmail.com>
2025-05-16 21:55:07 +0300

    feat: add role filed to user model

--- internal/infrastructure/postgres/user.go
+++ internal/infrastructure/postgres/user.go
@@ -1,8 +1,11 @@
 package postgres
 
+import "github.com/LavaJover/shvark-user-service/internal/domain"
+
 type UserModel struct {
-	ID				string `gorm:"primaryKey;type:uuid"`
-	Login			string `gorm:"unique;not null"`
-	Username		string `gorm:"unique;not null"`
-	PasswordHash	string `gorm:"not null"`
+	ID				string 		`gorm:"primaryKey;type:uuid"`
+	Login			string 		`gorm:"unique;not null"`
+	Username		string 		`gorm:"unique;not null"`
+	PasswordHash	string 		`gorm:"not null"`
+	Role			domain.Role	
 }
