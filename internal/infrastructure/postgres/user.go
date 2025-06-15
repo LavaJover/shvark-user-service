346ad785f83c8a7596eb873c08d5d4bf29e8c25c
bodya <lavajover@gmail.com>
2025-06-15 14:17:11 +0300

    feat: add CreatedAt, UpdatedAt fields to User Model

--- internal/infrastructure/postgres/user.go
+++ internal/infrastructure/postgres/user.go
@@ -1,8 +1,12 @@
 package postgres
 
+import "time"
+
 type UserModel struct {
 	ID				string `gorm:"primaryKey;type:uuid"`
 	Login			string `gorm:"unique;not null"`
 	Username		string `gorm:"unique;not null"`
 	PasswordHash	string `gorm:"not null"`
+	CreatedAt 		time.Time
+	UpdatedAt		time.Time
 }
